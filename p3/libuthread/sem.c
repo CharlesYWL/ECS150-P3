f3d7ffc4c61130c120c81a6da3137630eee14eb7
CharlesYWL <xlyin@ucdavis.edu>
2019-02-19 06:40:43 -0800

    try on make

--- p3/libuthread/sem.c
+++ p3/libuthread/sem.c
@@ -33,8 +33,8 @@ int sem_destroy(sem_t sem)
 	}else{
 		while(queue_length(sem->thread_list)>0){
 			queue_t *q;
-			quese_dequeue(sem->thread_list,q);
-			queue_delete(sem->thread_list,&q);
+			queue_dequeue(sem->thread_list,q);
+			queue_delete(sem->thread_list,*q);
 		}
 		free(sem->thread_list);
 		free(sem);
@@ -50,7 +50,7 @@ int sem_down(sem_t sem)
 	enter_critical_section();
 	if(sem == NULL) return -1;
 	while(sem->count == 0){ //block self while no resources aviliable
-		queue_enqueue(sem->thread_list,pthread_self());
+		queue_enqueue(sem->thread_list,(void*)pthread_self());
 		thread_block();
 	}
 	sem->count -= 1;
@@ -67,8 +67,8 @@ int sem_up(sem_t sem)
 	sem->count += 1;
 	//wake up first in line
 	void* rtnid;
-	if(quese_dequeue(sem->thread_list,rtnid))
-		thread_unblock(rtnid);
+	if(queue_dequeue(sem->thread_list,rtnid))
+		thread_unblock(&rtnid);
 	exit_critical_section();
 	return 0;
 }
@@ -77,13 +77,10 @@ int sem_getvalue(sem_t sem, int *sval)
 {
 	if(sem == NULL) return -1;
 	if (sem->count > 0) {
-		sval = sem->count;
+		*sval = sem->count;
 	}else {
-		sval = -1*queue_length(sem->thread_list);
+		*sval = -1*queue_length(sem->thread_list);
 	}
-	
-	
-
 	return 0;
 }
 
