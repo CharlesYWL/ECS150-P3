8e983e76988f43e11e0db41e98eacc8cc9e5150a
CharlesYWL <xlyin@ucdavis.edu>
2019-02-20 21:19:27 -0800

    success on mmap and protected

--- p3/libuthread/tps.c
+++ p3/libuthread/tps.c
@@ -8,10 +8,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <errno.h>
-#define __USE_MISC
-#define _POSIX_C_SOURCE 200112L
-#define _DEFAULT_SOURCE /* for MAP_ANONYMOUS */
-#include <bits/mman.h>
+#include <signal.h>
 #include <fcntl.h>
 #include "queue.h"
 #include "thread.h"
@@ -23,31 +20,44 @@ struct tps
 {
 	pthread_t pid;
 	//size_t length;
-	void* storage;
+	void *storage;
 };
 typedef struct tps *tps_t;
 
-int queue_equal(void* target1, void* target2){
+int queue_equal(void *target1, void *target2)
+{
 	//return 1 if same, 0 not same
-	return ((tps_t)target1)->pid == *(pthread_t*)target2 ? 1 : 0;
+	return ((tps_t)target1)->pid == *(pthread_t *)target2 ? 1 : 0;
 }
 
-tps_t tps_find(pthread_t target){
-	tps_t rtn ;
-	if(globalStore==NULL || !target){
+tps_t tps_find(pthread_t target)
+{
+	tps_t rtn = NULL;
+	if (globalStore == NULL || !target)
+	{
 		//fprintf(stderr,"invalid queue_t or target\n");
 		return NULL;
 	}
-	else{
-		queue_iterate(globalStore,queue_equal,(void*)&target,(void*)&rtn);
+	else
+	{
+		if (queue_iterate(globalStore, queue_equal, (void *)&target, (void *)&rtn))
+			return NULL;
 		return rtn;
 	}
-		
 }
 
 int tps_init(int segv)
 {
-		globalStore = queue_create();
+	/*	if (segv) {
+        struct sigaction sa;
+
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = SA_SIGINFO;
+        sa.sa_sigaction = segv_handler;
+        sigaction(SIGBUS, &sa, NULL);
+        sigaction(SIGSEGV, &sa, NULL);
+    }*/
+	globalStore = queue_create();
 }
 
 int tps_create(void)
@@ -55,44 +65,68 @@ int tps_create(void)
 	tps_t t = malloc(sizeof(struct tps));
 	//PROT_EXEC/READ/WRITE/NONE
 	//MAP_SHARED/FIXED/PRIVATE
-	void * newpage = mmap(NULL,TPS_SIZE,PROT_READ|PROT_READ,MAP_ANONYMOUS,-1,0);
+	void *newpage = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	perror("errno: ");
-	fprintf(stderr,"newpage: %p has been assigned\n",newpage);
+	fprintf(stderr, "newpage: %p has been assigned\n", newpage);
 	t->pid = pthread_self();
 	t->storage = newpage;
-	queue_enqueue(globalStore,t);
-
+	queue_enqueue(globalStore, t);
 }
 
 int tps_destroy(void)
 {
 	tps_t target = tps_find(pthread_self());
-	return munmap(target->storage,TPS_SIZE);
+	return munmap(target->storage, TPS_SIZE);
 }
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
 	tps_t target = tps_find(pthread_self());
-	memcpy(buffer,target->storage+offset,length);
+	if (!target || mprotect(target->storage, TPS_SIZE, PROT_READ) == -1)
+	{
+		perror("tps_read: mprotect: ");
+		return -1;
+	}
+	else
+	{
+		memcpy(buffer, target->storage + offset, length);
+		mprotect(target->storage, TPS_SIZE, PROT_NONE);
+		return 0;
+	}
 }
 
 int tps_write(size_t offset, size_t length, char *buffer)
 {
 	tps_t target = tps_find(pthread_self());
-	memcpy(target->storage,buffer+offset,length);
+	if (!target || mprotect(target->storage, TPS_SIZE, PROT_WRITE) == -1)
+	{
+		//if any failed
+		perror("tps_write: mprotect: ");
+		return -1;
+	}
+	else
+	{
+		memcpy(target->storage + offset, buffer, length);
+		perror("wirte: mmcpy");
+		mprotect(target->storage, TPS_SIZE, PROT_NONE);
+		return 0;
+	}
 }
 
 int tps_clone(pthread_t tid)
 {
 	tps_t self = tps_find(pthread_self());
 	tps_t target = tps_find(tid);
-	if(target == NULL|| self != NULL) 
+	if (target == NULL || self != NULL)
 		return -1;
-	else{
+	else
+	{
 		tps_create();
 		self = tps_find(pthread_self());
-		memcpy(self->storage,target->storage,TPS_SIZE);
+		mprotect(target->storage, TPS_SIZE, PROT_READ);
+		mprotect(self->storage, TPS_SIZE, PROT_WRITE);
+		memcpy(self->storage, target->storage, TPS_SIZE);
+		mprotect(target->storage, TPS_SIZE, PROT_NONE);
+		mprotect(self->storage, TPS_SIZE, PROT_NONE);
 	}
 }
-
-
